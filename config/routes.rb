e4864c1e0e02fae7b59022f85a27c04835df2ab1
Zach Young <zachjyoung@gmail.com>
2013-12-12 13:14:57 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Shouter::Application.routes.draw do
+  root to: 'homes#show', via: :get
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+end
---
+++ db/migrate/20131212171240_add_usersname_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsersnameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username
+  end
+end
